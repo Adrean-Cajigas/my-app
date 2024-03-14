895e37641589c2b3e20f52a110a60a8057da3445
Adrean Cajigas <adrean.cajigas@gmail.com>
2024-03-14 13:24:58 -0700

    feat: Set up Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <>
+    <h1 className="text-[10rem] font-[700]">Adrean Cajigas</h1>
     
-    </main>
+    </>
   );
 }
